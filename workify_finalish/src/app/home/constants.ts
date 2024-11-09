e8aba332ba869b48467554b82473fb635b76cd00
Taksh <dhabalia.taksh@gmail.com>
2024-11-10 02:22:33 +0530

    added resumes

--- workify_finalish/src/app/home/constants.ts
+++ workify_finalish/src/app/home/constants.ts
@@ -9,83 +9,73 @@ import { deepClone } from "lib/deep-clone";
 
 export const END_HOME_RESUME: Resume = {
   profile: {
-    name: "John Doe",
+    name: "Taksh Dhabalia",
     summary:
-      "Software engineer obsessed with building exceptional products that people love",
-    email: "hello@openresume.com",
-    phone: "123-456-7890",
-    location: "NYC, NY",
-    url: "linkedin.com/in/john-doe",
+      "Software engineer passionate about building exceptional products that solve real-world problems.",
+    email: "dhabalia.taksh@gmail.com",
+    phone: "+91 7715958053",
+    location: "Pune, Maharashtra",
+    url: "linkedin.com/in/taksh-dhabalia-2b6969202",
   },
   workExperiences: [
     {
-      company: "ABC Company",
-      jobTitle: "Software Engineer",
-      date: "May 2023 - Present",
+      company: "IIIT-Delhi",
+      jobTitle: "System Design and Embedded Software Developer Intern",
+      date: "Jan 2024 - Aug 2024",
       descriptions: [
-        "Lead a cross-functional team of 5 engineers in developing a search bar, which enables thousands of daily active users to search content across the entire platform",
-        "Create stunning home page product demo animations that drives up sign up rate by 20%",
-        "Write clean code that is modular and easy to maintain while ensuring 100% test coverage",
+        "Designed and implemented a water management system with app and cloud integration.",
+        "Developed hardware architecture, deployed on STM32 microcontrollers.",
+        "Built the project app in Dart using Flutter and integrated Firebase backend.",
       ],
     },
     {
-      company: "DEF Organization",
-      jobTitle: "Software Engineer Intern",
-      date: "Summer 2022",
+      company: "Team Bolt",
+      jobTitle: "Vice Lead, Electronics",
+      date: "June 2023 - Jan 2024",
       descriptions: [
-        "Re-architected the existing content editor to be mobile responsive that led to a 10% increase in mobile user engagement",
-        "Created a progress bar to help users track progress that drove up user retention by 15%",
-        "Discovered and fixed 5 bugs in the existing codebase to enhance user experience",
-      ],
-    },
-    {
-      company: "XYZ University",
-      jobTitle: "Research Assistant",
-      date: "Summer 2021",
-      descriptions: [
-        "Devised a new NLP algorithm in text classification that results in 10% accuracy increase",
-        "Compiled and presented research findings to a group of 20+ faculty and students",
+        "Led the electronics department in the FMAE Moto Student India competition, achieving top rankings.",
+        "Developed innovations like GPS tracking and SOS sensors for electric super bike.",
+        "Managed electrical systems and circuitry for GLVS and HVS.",
       ],
     },
   ],
   educations: [
     {
-      school: "XYZ University",
-      degree: "Bachelor of Science in Computer Science",
-      date: "Sep 2019 - May 2023",
-      gpa: "3.8",
+      school: "Maharashtra Institute of Technology - Pune",
+      degree: "BTech in Computer Science",
+      date: "Aug 2022 - June 2026",
+      gpa: "7.5",
       descriptions: [
-        "Won 1st place in 2022 Education Hackathon, 2nd place in 2023 Health Tech Competition",
-        "Teaching Assistant for Programming for the Web (2022 - 2023)",
-        "Coursework: Object-Oriented Programming (A+), Programming for the Web (A+), Cloud Computing (A), Introduction to Machine Learning (A-), Algorithms Analysis (A-)",
+        "Secured 91.7% in 10th ICSE.",
+        "Relevant coursework: Embedded Systems, Algorithms, Machine Learning.",
       ],
     },
   ],
   projects: [
     {
-      project: "OpenResume",
-      date: "Spring 2023",
+      project: "Tarzan",
+      date: "Aug 2024 - Present",
       descriptions: [
-        "Created and launched a free resume builder web app that allows thousands of users to create professional resume easily and land their dream jobs",
+        "Developing a portable module for autonomous driving in non-ADAS cars using deep learning and sensors.",
+        "Implemented steering, acceleration, and braking control with real-time MATLAB simulations.",
       ],
-    },
+    }
   ],
   skills: {
     featuredSkills: [
-      { skill: "HTML", rating: 4 },
-      { skill: "CSS", rating: 4 },
-      { skill: "Python", rating: 3 },
-      { skill: "TypeScript", rating: 3 },
-      { skill: "React", rating: 3 },
-      { skill: "C++", rating: 2 },
+      { skill: "Python", rating: 4 },
+      { skill: "C++", rating: 4 },
+      { skill: "Flutter", rating: 3 },
+      { skill: "TensorFlow", rating: 3 },
+      { skill: "OpenCV", rating: 3 },
     ],
     descriptions: [
-      "Tech: React Hooks, GraphQL, Node.js, SQL, Postgres, NoSql, Redis, REST API, Git",
-      "Soft: Teamwork, Creative Problem Solving, Communication, Learning Mindset, Agile",
+      "Tech: Embedded Systems, MATLAB, Node-RED, ESP-Now, LoRaWAN",
+      "Soft: Leadership, Communication, Problem Solving, Team Collaboration",
     ],
   },
   custom: {
-    descriptions: [],
+    descriptions: ["Led projects across cloud-based embedded systems and autonomous driving modules."],
   },
 };
 
